c927b5d272ab3a5378aa6f45b749a954c81c5abc
LydiahKamuyu <kamuyulydia@gmail.com>
2023-04-10 21:22:38 +0300

    A simple C-Calculator

---
+++ C-Calculator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+	float valueOne;
+	float valueTwo;
+	char operator;
+	float answer;
+
+	printf("Enter Calculation:\n");
+	scanf("%f %c %f", &valueOne, &operator, &valueTwo);
+}
